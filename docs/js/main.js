5286d56f57bcd9bbb39621497954c7831c9a02e3
Daniel Fariña <splitfirex@gmail.com>
2018-04-06 23:06:16 +0200

    Finalizado la primera pantalla

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,10 @@
-ReactDOM.render(<Cp2 hello="Helo world!"/>,document.getElementById("root"));
+//ReactDOM.render(<Cp2 hello="Helo world!"/>,document.getElementById("root"));
+
+document.getElementById("menuBtn").addEventListener("click", toggleMenu);
+
+
+
+function toggleMenu(){
+    var div = document.getElementById("modalMenu");
+    div.classList.add("modalOn");
+}
