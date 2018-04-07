a6831cb1b7089acbbd6afe5df2efcd9f46745505
Daniel Fariña <splitfirex@gmail.com>
2018-04-07 21:22:10 +0200

    -Mas cosas bonitas

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,34 @@
 //ReactDOM.render(<Cp2 hello="Helo world!"/>,document.getElementById("root"));
 
-document.getElementById("menuBtn").addEventListener("click", toggleMenu);
+document.getElementById("menuBtnOpen").addEventListener("click", openMenu);
 
+//document.getElementById("menuBtnOpen").addEventListener("click", closeMenu);
 
+function openMenu(){
+    var div = document.getElementById("slideMenu");
+    div.classList.add("menuOn");
 
-function toggleMenu(){
-    var div = document.getElementById("modalMenu");
-    div.classList.add("modalOn");
+    document.getElementById("menuBtnOpen").removeEventListener("click",openMenu);
+    document.getElementById("menuBtnOpen").addEventListener("click", closeMenu);
 }
+
+function closeMenu(){
+    var div = document.getElementById("slideMenu");
+    div.classList.remove("menuOn");
+
+    document.getElementById("menuBtnOpen").addEventListener("click",openMenu);
+    document.getElementById("menuBtnOpen").removeEventListener("click", closeMenu);
+}
+
+function displaySignIn(){
+    var div = document.getElementById("container");
+    document.getElementById('container').style.display= 'none' ;
+    document.getElementById('modal').classList.add("show");
+}
+
+var itemClickClass = document.getElementsByClassName("itemClick");
+
+for (var i = 0; i < itemClickClass.length; i++) {
+    itemClickClass[i].addEventListener('click', closeMenu, false);
+}
+
