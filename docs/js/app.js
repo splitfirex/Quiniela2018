a1ea807d81989408aec7dcbf292a64786d62bce4
dfarina <dfarina@idom.com>
2018-04-09 17:32:39 +0200

    - Mas cambios

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,20 +1,25 @@
 class App extends React.Component {
 
     render() {
+
         return [
-            <div  key="header" className="header" id="header">
-                <img src="img/woldcup.svg" width="100%" />
-            </div>,
-            <Notification key="notification"/>,
-            <Quiniela key="quiniela"/>,
-            <Content key="content"/>,
-            <Groups key="groups"/>,
-            <Matches key="matches"/>,
-            //<PlayerMatches key="PlayerMatches"/>,
-            <SlideMenu key="SlideMenu"/>
+            <SlideMenu key="SlideMenu" />,
+            <div id="container" class="container">
+                <div className="header" id="header">
+                    <img src="img/woldcup.svg" width="100%" />
+                </div>,
+                <Notification />
+                <Quiniela />
+                <Content />
+                <Groups />
+                <Matches />
+                <PlayerMatches />
+
+            </div>
         ]
+
     }
 }
 
 
-ReactDOM.render(<App />, document.getElementById("container"));
+ReactDOM.render(<App />, document.getElementById("root"));
