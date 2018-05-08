d04966bff34a41555099acdce58bee9115f1a0d5
dfarina <dfarina@idom.com>
2018-05-08 17:33:27 +0200

    Mas cambios

--- src/main/java/quiniela/repository2/DOMGroupCustomRepository.java
+++ src/main/java/quiniela/repository2/DOMGroupCustomRepository.java
@@ -1,7 +1,12 @@
 package quiniela.repository2;
 
 import org.springframework.stereotype.Repository;
+import quiniela.utils.dom.DOMMatch;
+
+import java.util.List;
 
 @Repository
-public interface DOMGroupCustomRepository {
+public interface DOMGroupCustomRepository{
+
+    List<DOMMatch> getMatchesByIdPlayerAndIdLadder(Long idPlayer, Long IdLadder);
 }
